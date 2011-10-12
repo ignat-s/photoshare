023255903f9a4254a4088e036ad0316efb418911
Ignat Shcheglovskyi <ignat.scheglovskiy@gmail.com>
2011-10-12 10:57:41 +0300

    Add deploy script

---
+++ bin/deploy.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# Initializing dirs
+source_dir=$1
+target_dir=$2
+
+assert_dir()
+{
+	if [ ! -d "$@" ]; then
+		log_msg "$@ is not a directory"
+		exit
+	fi
+}
+
+log_msg() { echo $@; }
+log_usage_msg() { log_msg "Usage: deploy.sh SOURCE_DIR TARGET_DIR"; }
+
+
+if [ ! $source_dir -o ! $target_dir ]; then
+	log_usage_msg
+	exit
+fi
+
+assert_dir $source_dir
+rsync -avz --exclude=".git" --exclude="app/cache/*" --exclude="app/logs/*"  --exclude="web/bundles/*" --exclude=".idea" --exclude="photostorage/*"  $source_dir/ $target_dir/
